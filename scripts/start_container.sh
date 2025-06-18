cb5e296c6db4c5c76b22c63fce8ef7057b126352
Arijit Chakraborty <arijitchakraborty691@gmail.com>
2025-06-18 12:32:20 +0000

    add them

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-# Pull the Docker image from Docker Hub
+# Pull the Docker image from Docker HuB
 docker pull arijit2001/simple-python-flask-app:latest
 
 # Run the Docker image as a container
